336e7d0d44550d5596eb2056c5670260b2029dd4
wangfeng <hustbroventure@163.com>
2015-10-27 20:58:20 +0800

    创建
    此项目用于组织文件结构。预先放入了一些必要的类库和类别

---
+++ HBProjectDemo/HBProjectDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HBProjectDemo
+//
+//  Created by wangfeng on 15/10/27.
+//  Copyright (c) 2015年 HustBroventurre. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HBProjectDemo/HBProjectDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HBProjectDemo
+//
+//  Created by wangfeng on 15/10/27.
+//  Copyright (c) 2015年 HustBroventurre. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
